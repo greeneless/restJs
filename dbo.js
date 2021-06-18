fcc446d75c8a3f0a7fbc32e6735d2cc837004cf7
Michael Greene <mgreene@mspbuilder.com>
2021-06-18 15:05:12 -0400

    Added very rough dbInsert func

--- dbo.js
+++ dbo.js
@@ -12,6 +12,25 @@ async function dbSelect(config, table, field, value) {
         }
     }
 
+
+async function dbInsert(config, table, columns, values) {
+        if (!Array.isArray(columns)) { return 'columns is not an arry' }
+        if (!Array.isArray(values)) { return 'values is not an array' }
+        if (columns.length !== values.length) { return 'array lengths do not match' }
+        try {
+                columns = columns.join(',') 
+                values = values.join(',')
+                const pool = await mssql.connect(config)
+                const record = await pool.request().query(
+                        "INSERT INTO " + table + "(" + columns + ") VALUES (" + values + ")")
+                pool.close()
+    
+                return record.recordsets
+        } catch (error) {
+                throw error
+        }
+    }
+
 module.exports = {
         dbSelect
 }
